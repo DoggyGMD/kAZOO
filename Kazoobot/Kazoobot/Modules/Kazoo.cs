2ed86739372936b327c884ee0485aac85598c9bb
Ale De La Cruz <alejandrodelacruztejeda@gmail.com>
2017-12-16 10:27:29 -0400

    well i did nothhing :v

---
+++ Kazoobot/Kazoobot/Modules/Kazoo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace Kazoobot.Modules
+{
+    public class Kazoo : ModuleBase<SocketCommandContext>
+    {
+        [Command("kazoo"), Alias("K")]
+        public async Task kazooooo(SocketUser User)
+        {
+            await ReplyAsync("Kazzo'd "+User.Username);
+
+            var DMs = await User.GetOrCreateDMChannelAsync();
+
+            await DMs.SendMessageAsync("https://www.youtube.com/watch?v=oPqigBVGpOE");
+        }
+        [Command("kazoo"), Alias("K")]
+        public async Task kazoooo()
+        {
+            await ReplyAsync("https://www.youtube.com/watch?v=oPqigBVGpOE");
+        }
+        [Command("suc")]
+        public async Task succccc()
+        {
+            await ReplyAsync("SUCK");
+        }
+
+    } 
+}  
