1948a1ee9a423915ae34ba4097593ca1650d2c7b
Ale De La Cruz <alejandrodelacruztejeda@gmail.com>
2017-12-16 11:18:21 -0400

    new command !hi

---
+++ Kazoobot/Kazoobot/Modules/greetings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+
+
+namespace Kazoobot.Modules
+{
+    public class greetings : ModuleBase<SocketCommandContext>
+        {
+        [Command("hi")]
+        public async Task hi()
+        {
+            await ReplyAsync("hello!");
+        }
+
+
+    }
+}
